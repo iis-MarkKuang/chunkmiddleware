d7feb08a2cce2174936fcb2721af67c65ce63066
KuangYuan <kuangyuan2014@163.com>
2017-04-24 16:46:05 +0800

    updated.

--- chunk.py
+++ chunk.py
@@ -33,7 +33,7 @@ class SETHandler(BaseHTTPRequestHandler):
             # Send the html message
             self.wfile.write(str(ThreadedHTTPServer.completion))
         else:
-            self.send_response(200)
+            self.send_response(404)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             # Send the html message
@@ -91,7 +91,7 @@ class SETHandler(BaseHTTPRequestHandler):
                 result["result"] = "Finished"
                 self.wfile.write(json.dumps(result))
         else:
-            self.send_response(200)
+            self.send_response(404)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             # Send the html message
